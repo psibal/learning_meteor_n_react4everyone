81f437f33af63096b2ea767b5519fafa64f9864b
Rafael Rios Saavedra <rafael.rios.saavedra@gmail.com>
2016-08-06 13:51:23 +0200

    Video 5: Getting Data From Meteor With TrackerReact, se recupera un dato de la BD

--- App.jsx
+++ App.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import TrackerReact from 'meteor/ultimatejs:tracker-react';
+
 
 Resolutions = new Mongo.Collection("resolutions");
 
-export default class App extends React.Component {
+export default class App extends TrackerReact(React.Component) {
+
+  resolutions() {
+  	return Resolutions.find().fetch();
+  }
+
   addResolution(event) {
      event.preventDefault();
 
@@ -18,12 +25,22 @@ export default class App extends React.Component {
   }
 
   render () {
+  	let res =  this.resolutions();
+
+    if (res.length < 1) {
+    	return(<div> Loading</div>)
+    }
+    console.log(res);
+
   	return (
   		<div>
   		  <h1>My resolutions</h1>
   		  <form className="new-resolution" onSubmit={this.addResolution.bind(this)}>
   		     <input type="text" ref="resolution" placeholder="Finish React Meteor Series" />
   		  </form>
+  		  <div>
+  		  {res[0].text}
+  		  </div>
   		</div>
   	)
   }
